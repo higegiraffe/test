19410aca6eae955387cdd3b5a89cd075619a1407
higegiraffe <yukinko@kk.iij4u.or.jp>
2015-06-17 01:18:36 +0900

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "起動しました"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonaction(sender: AnyObject) {
+        label.text = "ボタンが押されました"
+    }
+    
 }
 
